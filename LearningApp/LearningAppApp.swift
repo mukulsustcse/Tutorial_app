d1434b0d6a43a89df1ea49f6e58a41d2c4160baa
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-10 17:21:25 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by M. A. Alim Mukul on 10.09.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
